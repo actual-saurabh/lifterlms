2bd77b39aba3e0eaf9d16fe04f0d335ccaac5724
Saurabh Shukla <saurabh@yapapaya.com>
2019-11-25 23:21:14 +0530

    Conditionally load toolbar button

--- includes/modules/certificate-builder/includes/admin/class-llms-certificate-post-table.php
+++ includes/modules/certificate-builder/includes/admin/class-llms-certificate-post-table.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Post Table extensions for Certificate Builder
+ * Post Table extensions for Certificate Builder.
  *
  * @package LifterLMS/Modules/Certificate_Builder
  *
@@ -10,25 +10,27 @@
 
 defined( 'ABSPATH' ) || exit;
 
-
 /**
  * Handles post editor & post table modifications.
  *
- * @since    [version]
+ * @since [version]
  */
 class LLMS_Certificate_Post_Table {
 
 	/**
-	 * Constructor
+	 * Constructor.
 	 *
-	 * Hooks editor related modifications to Certificate post type.
-	 *
-	 * @since    [version]
+	 * @since [version]
 	 */
 	public function __construct() {
 		$this->hook();
 	}
 
+	/**
+	 * Hooks editor related modifications to Certificate post type.
+	 *
+	 * @since [version]
+	 */
 	public function hook() {
 		// hook build link to posts table.
 		add_filter( 'post_row_actions', array( $this, 'build_action' ), 10, 2 );
@@ -42,29 +44,34 @@ class LLMS_Certificate_Post_Table {
 	 *
 	 * @return array
 	 *
-	 * @since    [version]
+	 * @since [version]
 	 */
 	public function build_action( $actions, $post ) {
 
 		// Only load for certificates and for appropriate permissions.
-		if ( 'llms_certificate' === $post->post_type && current_user_can( 'edit_post', $post->ID ) ) {
-
-			// don't show on trashed certificates
-			if ( in_array( $post->post_status, array( 'trash', 'llms-legacy' ) ) ) {
-				return $actions;
-			}
-
-			// Get the build url.
-			$build_url = llms_certificate_build_url( $post->ID );
-
-			// Build action.
-			$build_action = array(
-				'build' => sprintf( '<a href="%1$s">%2$s</a>', $build_url, __( 'Build', 'lifterlms' ) ),
-			);
-
-			// prepend build url to post actions.
-			$actions = $build_action + $actions;
+		if ( 'llms_certificate' !== $post->post_type || ! current_user_can( 'edit_post', $post->ID ) ) {
+			return $actions;
 		}
+
+		// don't show on trashed certificates
+		if ( in_array( $post->post_status, array( 'trash', 'llms-legacy' ) ) ) {
+			return $actions;
+		}
+
+		if ( LLMS_Certificate_Migrator::is_legacy( $post->ID ) ) {
+			return $actions;
+		}
+
+		// Get the build url.
+		$build_url = llms_certificate_build_url( $post->ID );
+
+		// Build action.
+		$build_action = array(
+			'build' => sprintf( '<a href="%1$s">%2$s</a>', $build_url, __( 'Build', 'lifterlms' ) ),
+		);
+
+		// prepend build url to post actions.
+		$actions = $build_action + $actions;
 
 		return $actions;
 	}
